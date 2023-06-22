d17e81ad8b8cfc94c47897b653eae98feb9a7371
AE SChan <abigailsinchan@gmail.com>
2023-06-22 18:12:57 -0400

    structure and questions

---
+++ asset/js/question.js
@@ -0,0 +1,102 @@
+let quiz = [
+    {
+        question: "What is an example of a float?",
+        option: [
+            "1",
+            "60",
+            "300",
+            ".9",
+        ],
+        answer: 4,
+    },
+    {
+        question: "What is an example of a string?",
+        option: [
+            "3",
+            "\"hello\"",
+            "true",
+            "false",
+        ],
+        answer: 2,
+    },
+    {
+        question: "What does boolean stand for?",
+        option: [
+            "true and false",
+            "7",
+            "if statement",
+            "\"hello\"",
+        ],
+        answer: 1,
+    },
+    {
+        question: "What combines a string together?",
+        option: [
+            "variable",
+            "function",
+            "concatenation",
+            "divide",
+        ],
+        answer: 3,
+    },
+    {
+        question: "Which one does NOT create a variable?",
+        option: [
+            "let",
+            "const",
+            "var",
+            "NaN",
+        ],
+        answer: 4,
+    },
+    {
+        question: "What is a scope?",
+        option: [
+            "limits where a variable exist",
+            "let us combine boolean values",
+            "chooses number between 0 & 1",
+            "behaves just like true",
+        ],
+        answer: 1,
+    },
+    {
+        question: "What is a function?",
+        option: [
+            "selects random moves",
+            "groups multiple values together",
+            "JavaScript Object Notation",
+            "lets us reuse a code",
+        ],
+        answer: 4,
+    },
+    {
+        question: "What is an object for?",
+        option: [
+            "selects random moves",
+            "groups multiple values together",
+            "JavaScript Object Notation",
+            "lets us reuse a code",
+        ],
+        answer: 2,
+    },
+    {
+        question: "Which does the local storage only store?",
+        option: [
+            "object",
+            "booleans",
+            "strings",
+            "numbers",
+        ],
+        answer: 3,
+    },
+    {
+        question: "What models the webpage",
+        option: [
+            "JSON",
+            "JQuery",
+            "DOM",
+            "API",
+        ],
+        answer: 3,
+    },
+]
